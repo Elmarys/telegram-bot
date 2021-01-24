f71669f06b821e5f36f638e0c252c65f771deb59
Ilya Aleshin <ialeshin@gmail.com>
2021-01-25 00:23:00 +0300

    Code separeted into 3 different files for classes and constants. Created error handler

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,19 +1,11 @@
 import telebot
-import json
-import requests
+from utils import ConvertionError, CriptoConverter
+from config import TOKEN, keys
 
-
-TOKEN = '1563086294:AAEJ38N5WrYyl6xC8JzVTMuw3Z3Bdc2dRQA'
 
 bot = telebot.TeleBot(TOKEN)
 
-keys = {
-    'доллар':'USD',
-    'евро':'EUR',
-    'биткоин':'BTC'
-}
 
-# Обрабатываются все сообщения содержащие команды '/start' or '/help'.
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message):
     text = 'Чтобы начать работу, введите текст в следующем формате:\n' \
@@ -21,25 +13,35 @@ def handle_start_help(message):
            'Показать список доступных валют: /currencies'
     bot.reply_to(message, text)
 
+
 # Выводит список доступных валют
 @bot.message_handler(commands=['currencies'])
 def currencies(message):
     text = 'Доступные валюты:'
     for key in keys.keys():
-        text = '\n'.join((text,key))
+        text = '\n'.join((text, key))
     bot.reply_to(message, text)
 
 
-
+# Конвертер
 @bot.message_handler(content_types=['text'])
 def convert(message):
-    quote, base, amount = message.text.split(' ')
-    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={keys[quote]}&tsyms={keys[base]}')
-    global_base = json.loads(r.content)[keys[base]]
-    res = global_base * int(amount)
-    print(type(global_base))
-    text = f'Цена {amount} {keys[quote]} в {keys[base]} - {res}'
-    bot.send_message(message.chat.id, text)
+    try:
+        values = message.text.split(' ')
+        if len(values) != 3:
+            raise ConvertionError('Введено некорректное количество параметров!')
+
+        base, quote, amount = values
+        res = CriptoConverter.convert(quote, base, amount)
+    except ConvertionError as e:
+        bot.reply_to(message, f'Ошибка пользователя\n{e}')
+    except Exception as e:
+        bot.reply_to(message,f'Не удалось обработать команду\n{e}')
+    else:
+        text = f'Цена {amount} {keys[base]} в {keys[quote]} - {res}'
+        bot.send_message(message.chat.id, text)
 
 
 bot.polling(none_stop=True)
+
+
---
+++ utils.py
@@ -0,0 +1,32 @@
+import requests
+import json
+from config import keys
+
+
+class ConvertionError(Exception):
+    pass
+
+
+class CriptoConverter():
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+        if quote == base:
+            raise ConvertionError('Базовая и котируемая валюта совпадают.')
+        try:
+            quote_ticker = keys[quote]
+        except:
+            raise ConvertionError(f'Не удалось обработать валюту {quote}.')
+        try:
+            base_ticker = keys[base]
+        except:
+            raise ConvertionError(f'Не удалось обработать валюту {base}.')
+        try:
+            amount = float(amount)
+        except:
+            raise ConvertionError(f'Не удалось обработать сумму в котируемой валюте {amount}.')
+
+        r = requests.get(f'https://api.exchangeratesapi.io/latest?base={base_ticker}&symbols={quote_ticker}')
+        global_base = json.loads(r.content)['rates'][keys[quote]]
+        res = global_base * int(amount)
+
+        return res
